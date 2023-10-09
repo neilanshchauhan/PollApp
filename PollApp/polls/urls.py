b05a069a32f45ef4ac83a6f3bfd870fab159a739
Neilansh Chauhan <neilanshchauhan4@gmail.com>
2023-10-10 00:50:20 +0530

    Create poll views

--- PollApp/polls/urls.py
+++ PollApp/polls/urls.py
@@ -5,4 +5,6 @@ app_name = 'polls'
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('<int:question_id>/',views.detail,name='detail'),
+    path('<int:question_id>/',views.results,name='results'),
 ]
--- PollApp/polls/views.py
+++ PollApp/polls/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404
 from .models import Question,Choices
 
 # Create your views here.
@@ -7,3 +9,15 @@ def index(request):
     latest_questions = Question.objects.order_by('-published')[:5]
     context = {'latest_questions':latest_questions}
     return render(request,'polls/index.html',context)
+
+def detail(request,question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    
+    return render(request,'polls/results.html',{'question':question})
+
+def results(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,'polls/results.html',{'question':question})
